f1f94dcd331afb7cf22c987f899dc0ef49e757f7
Mohamed Ayman Saad <mohamed336699as55@gmail.com>
2026-09-16 22:31:15 +0300

    Secure and fix geocoding module

--- just_folder/geocode.js
+++ just_folder/geocode.js
@@ -1,33 +1,40 @@
-
 const request = require("request");
 require("dotenv").config();
 
 const geocode = (address, callback) => {
-
     const geocodeUrl =
         "https://api.mapbox.com/geocoding/v5/mapbox.places/" +
-        address +
+        encodeURIComponent(address) +
         ".json?access_token=" +
         process.env.MAPBOX_TOKEN;
 
+    if (!process.env.MAPBOX_TOKEN) {
+        return callback("MAPBOX_TOKEN is missing. Add it to your .env file", undefined);
+    }
+
     request({ url: geocodeUrl, json: true }, (error, response) => {
-
         if (error) {
-            callback("unable to connect geocode service", undefined);
-
-        } else if (response.body.message) {
-            callback(response.body.message, undefined);
-
-        } else if (response.body.features.length == 0) {
-            callback("Unable to find location", undefined);
-
-        } else {
-
-            callback(undefined, {
-                longitude: response.body.features[0].center[0],
-                latitude: response.body.features[0].center[1]
-            });
+            return callback("unable to connect geocode service", undefined);
         }
+
+        if (!response || !response.body) {
+            return callback("invalid response from geocode service", undefined);
+        }
+
+        if (response.body.message) {
+            return callback(response.body.message, undefined);
+        }
+
+        if (!response.body.features || response.body.features.length === 0) {
+            return callback("Unable to find location", undefined);
+        }
+
+        const location = response.body.features[0];
+
+        callback(undefined, {
+            longitude: location.center[0],
+            latitude: location.center[1]
+        });
     });
 };
 
